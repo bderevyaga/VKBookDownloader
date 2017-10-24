9c511a105b8c2cb7712473502de3d34fe0877020
bderevyaga <bogdan.derevyaga@gmail.com>
2017-10-24 08:36:19 +0300

    add get book

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 
 	"./file"
 	"./vk"
@@ -27,16 +28,18 @@ type Message struct {
 
 var nilDoc = Doc{}
 
-func main() {
-	fmt.Println("Set https proxy")
-	os.Setenv("HTTPS_PROXY", "https://193.37.152.6:3128")
+const access_token = "463cb4750555a2d9609f220bc056c9da82b8645572e9b6b6a766f63551307f5a74cbfc689b12a08b6bcf9"
+
+func getBook(offset int) int {
+	counter := 10
 
 	parameters := make(map[string]string)
 	parameters["domain"] = "proglib"
 	parameters["query"] = "#book@proglib"
-	parameters["count"] = "100"
+	parameters["count"] = strconv.Itoa(counter)
+	parameters["offset"] = strconv.Itoa(offset)
 	parameters["v"] = "5.68"
-	parameters["access_token"] = "463cb4750555a2d9609f220bc056c9da82b8645572e9b6b6a766f63551307f5a74cbfc689b12a08b6bcf9"
+	parameters["access_token"] = access_token
 
 	fmt.Println("Get 'wall.search' request")
 	resp, errVk := vk.Request("wall.search", parameters)
@@ -46,16 +49,24 @@ func main() {
 
 	var message Message
 
-	fmt.Println("Decode request")
+	fmt.Println("Decode request \n")
+
 	errJson := json.Unmarshal(resp, &message)
 	if errJson != nil {
 		fmt.Println("JSON error:", errJson)
 	}
 
+	//count := message.Response.Count
+	count := 9
+
+	if offset == 0 {
+		fmt.Println("Elements count:", count)
+	}
+
 	for _, element := range message.Response.Items {
 		for _, element := range element.Attachments {
 			if element.Doc != nilDoc {
-				fmt.Println("Download " + element.Doc.Title + "...")
+				fmt.Println("Download:", element.Doc.Title)
 
 				errDownload := file.Download(element.Doc.Title, element.Doc.URL)
 				if errDownload != nil {
@@ -66,6 +77,21 @@ func main() {
 			}
 		}
 	}
+
+	fmt.Println("Donloads", offset, "for", count, "elements... \n")
+
+	if count > offset {
+		return getBook(offset + counter)
+	}
+
+	return offset
+}
+
+func main() {
+	fmt.Println("Set https proxy")
+	os.Setenv("HTTPS_PROXY", "https://193.37.152.6:3128")
+
+	getBook(0)
 
 	fmt.Println("Done ALL...")
 }
