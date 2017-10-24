6d994f79e1efb15c560e137e8a4cc6ad46823e51
bderevyaga <bogdan.derevyaga@gmail.com>
2017-10-24 20:28:32 +0300

    add db

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
 
+	_ "github.com/mattn/go-sqlite3"
+
 	"./file"
 	"./vk"
 )
+
+type DB struct {
+	Id int
+	Domain string
+	Query string
+	LasId int
+}
 
 type Doc struct {
 	URL   string
@@ -19,6 +29,7 @@ type Message struct {
 	Response struct {
 		Count int
 		Items []struct {
+			Id int
 			Attachments []struct {
 				Doc Doc
 			}
@@ -30,18 +41,18 @@ var nilDoc = Doc{}
 
 const access_token = "463cb4750555a2d9609f220bc056c9da82b8645572e9b6b6a766f63551307f5a74cbfc689b12a08b6bcf9"
 
-func getBook(offset int) int {
-	counter := 10
+func getBook(offset int, db DB) int {
+	counter := 50
 
 	parameters := make(map[string]string)
-	parameters["domain"] = "proglib"
-	parameters["query"] = "#book@proglib"
+	parameters["domain"] = db.Domain
+	parameters["query"] = db.Query
 	parameters["count"] = strconv.Itoa(counter)
 	parameters["offset"] = strconv.Itoa(offset)
 	parameters["v"] = "5.68"
 	parameters["access_token"] = access_token
 
-	fmt.Println("Get 'wall.search' request")
+	fmt.Print("Get 'wall.search' request \n")
 	resp, errVk := vk.Request("wall.search", parameters)
 	if errVk != nil {
 		fmt.Println("VK error:", errVk)
@@ -49,21 +60,27 @@ func getBook(offset int) int {
 
 	var message Message
 
-	fmt.Println("Decode request \n")
+	fmt.Print("Decode request \n\n")
 
 	errJson := json.Unmarshal(resp, &message)
 	if errJson != nil {
 		fmt.Println("JSON error:", errJson)
 	}
 
-	//count := message.Response.Count
-	count := 9
+	count := message.Response.Count
 
 	if offset == 0 {
-		fmt.Println("Elements count:", count)
+		statement, _ := database.Prepare("UPDATE groups SET las_id = ? WHERE id = ?")
+		statement.Exec(message.Response.Items[0].Id, db.Id)
+
+		fmt.Print("Elements count:", count, "\n\n")
 	}
 
 	for _, element := range message.Response.Items {
+		if element.Id == db.LasId {
+			return offset
+		}
+
 		for _, element := range element.Attachments {
 			if element.Doc != nilDoc {
 				fmt.Println("Download:", element.Doc.Title)
@@ -73,25 +90,50 @@ func getBook(offset int) int {
 					fmt.Println("Download error:", errDownload)
 				}
 
-				fmt.Println("Done download \n")
+				fmt.Print("Done download \n\n")
 			}
 		}
 	}
 
-	fmt.Println("Donloads", offset, "for", count, "elements... \n")
+	fmt.Print("Donloads", offset, "for", count, "elements... \n\n")
 
 	if count > offset {
-		return getBook(offset + counter)
+		return getBook(offset + counter, db)
 	}
 
 	return offset
 }
 
-func main() {
-	fmt.Println("Set https proxy")
-	os.Setenv("HTTPS_PROXY", "https://193.37.152.6:3128")
 
-	getBook(0)
+var database *sql.DB
+
+func main() {
+	fmt.Print("Set https proxy \n\n")
+	os.Setenv("HTTPS_PROXY", "https://193.37.152.6:3128")
+	
+	fmt.Print("Open DB \n\n")
+	database, _ = sql.Open("sqlite3", "./parse.db")
+
+	var datasDB []DB
+
+	rows, _ := database.Query("SELECT id, domain, query, las_id FROM groups")
+	for rows.Next() {
+		datas := new(DB)
+		rows.Scan(&datas.Id, &datas.Domain, &datas.Query, &datas.LasId)
+
+		datasDB = append(datasDB, *datas)
+	}
+
+	for _, db := range datasDB {
+		fmt.Println("Domain: ", db.Domain, " Query: ", db.Query)
+		getBook(0, db)
+	}
 
 	fmt.Println("Done ALL...")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
